d6e3e7fa68865e78f4357a60494e5446c66ef463
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-06-25 03:48:08 -0300

    fix: update authUrl in production environment configuration

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://parsley-rely-snub.ngrok-free.dev',
-  authUrl: 'https://crazy-candles-wait.loca.lt',
+  authUrl: 'https://whole-bottles-deny.loca.lt',
 };
