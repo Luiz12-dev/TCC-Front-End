206499b4b48b355ed54307136831d343bbed4941
Luiz12-dev <aluizotaviodealmeida@gmail.com>
2026-06-25 03:18:56 -0300

    feat: update auth interceptor to include authorization header and modify environment URLs

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -2,16 +2,24 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   let token = null;
+
   if (typeof window !== 'undefined') {
     token = localStorage.getItem('token');
   }
-  
+
+  // 1. Preparamos os cabeçalhos. O Ngrok SEMPRE vai.
+  let customHeaders = req.headers.set('ngrok-skip-browser-warning', 'true');
+
+  // 2. Se o token existir, adicionamos ele também.
   if (token) {
-    const clonedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    });
-    return next(clonedReq);
+    customHeaders = customHeaders.set('Authorization', `Bearer ${token}`);
   }
-  
-  return next(req);
+
+  // 3. Clonamos a requisição UMA ÚNICA VEZ com os cabeçalhos finais.
+  const clonedReq = req.clone({
+    headers: customHeaders,
+  });
+
+  // 4. Enviamos para o servidor.
+  return next(clonedReq);
 };
